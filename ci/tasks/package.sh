c8593b0daee429841a9ec38ec1a1f125daf6716e
Nikam <516152@AMB00678.local>
2017-07-21 16:20:02 +0530

    jar file changes

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -7,14 +7,14 @@ pushd apps-movie-fun-code-source
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+jar_count=`find apps-movie-fun-code-source/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find apps-movie-fun-code-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.war \;
+find apps-movie-fun-code-source/target -type f -name *.jar -exec cp "{}" package-output/moviefun.war \;
 
 echo "Done packaging"
 exit 0
